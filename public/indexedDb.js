0b43605374c6857e5d03e4daab681b655ac545de
Brandon <hutchingsonb@gmail.com>
2021-06-01 12:03:35 -0400

    added .onupgradneeded to indexedDb

--- public/indexedDb.js
+++ public/indexedDb.js
@@ -3,3 +3,18 @@ let budgetVersion;
 
 //Create new db request
 const request = indexedDB.open('BudgetDB', budgetVersion || 21);
+
+request.onupgradeneeded = event => {
+    console.log('Upgrade neede in IndexDB.');
+
+    const { oldVersion } = event;
+    const newVersion = event.newVersion || db.version;
+
+    console.log(`DB has been updated from version ${oldVersion} to ${newVersion}`);
+
+    db = event.target.result;
+
+    if(db.objectStoreNames.length === 0) {
+        db.createObjectStore('BudgetStore', { autoIncrement: true });
+    }
+};
